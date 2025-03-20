cc06230df46dbdde42498d8ad0ec82b5488a7600
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2025-03-21 00:56:34 +0530

    create timer and waitlist (with bugs)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import FlipTimer from '@/components/FlipTimer'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import logo from '@/../public/fein-ai-logo.svg'
+import JoinWaitlist from '@/components/JoinWaitlist'
 
 export default function Home() {
+
+  const endDate = new Date('2025-04-01T00:00:00+05:30')
   return (
-    <div>
-      Home
+    <div className='flex flex-col items-center justify-center gap-10 h-screen'>
+      <Image className='absolute sm:left-10 top-5 left-5' src={logo} alt="fein-ai" width={150} />
+      <h1 className='text-2xl sm:text-4xl font-bold'>Countdown to April 1, 2025</h1>
+      <FlipTimer endDate={endDate} />
+      <JoinWaitlist />
     </div>
   )
 }
---
+++ src/components/JoinWaitlist.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export default function JoinWaitlist() {
+
+    const [email, setEmail] = useState('');
+    const [open, setOpen] = useState(false);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+    }
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+                <Button size="lg">Join the Waitlist</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>Enter Email address</DialogTitle>
+                    <DialogDescription>
+                        Enter your email address to join the waitlist. We will send you an email when we are ready to launch.
+                    </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="name" className="text-right">
+                            Email
+                        </Label>
+                        <Input id="email" value={email} placeholder="fein.ai@fein.ai.com" onChange={handleChange} className="col-span-3" />
+                    </div>
+                </div>
+                <DialogFooter className="w-full flex items-center justify-start">
+                    <Button type="submit" onClick={() => setOpen(false)}>
+                        Join Waitlist
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
